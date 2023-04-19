a6a50ab5d2d49f674aa9bb06f1d13cdbca768ea9
suah lee <suah216@gmail.com>
2023-04-19 19:17:29 +0900

    first

---
+++ main.js
@@ -0,0 +1,17 @@
+let currencyRatio = {
+    USD:{
+        KRW: 1335.61,
+        USD:1,
+        VND:23512.50
+    },
+    KRW:{
+        KRW: 1,
+        USD:0.00075,
+        VND:17.61
+    },
+    VND:{
+        KRW: 0.057,
+        USD:0.000043,
+        VND:1
+    }
+}
